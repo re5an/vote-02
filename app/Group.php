86d55886183c5044d2243c0d724216602f2b0411
re5an <39256403+re5an@users.noreply.github.com>
2020-12-14 21:21:40 +0330

    Group

--- app/Group.php
+++ app/Group.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
